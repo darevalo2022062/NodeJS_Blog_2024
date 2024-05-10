0bf45ad1813c583a898de3bbffa4b00f7e19f42f
David <darevalo-2022062@kinal.edu.gt>
2024-05-10 16:26:54 -0600

    Servidor del proyecto montado

---
+++ configs/server.js
@@ -0,0 +1,41 @@
+'use strict'
+
+import express from 'express';
+import cors from 'cors';
+import helmet from 'helmet';
+import morgan from 'morgan';
+import { conectarDB } from './mongo.js';
+
+class Server {
+    constructor() {
+        this.app = express();
+        this.port = process.env.PORT;
+
+        this.middlewares();
+        this.conectarDB();
+        this.routes();
+    }
+
+    async conectarDB() {
+        await conectarDB();
+    }
+
+    middlewares() {
+        this.app.use(cors());
+        this.app.use(express.json());
+        this.app.use(helmet());
+        this.app.use(morgan('dev'));
+    }
+
+    routes() {
+
+    }
+
+    listen() {
+        this.app.listen(this.port, () => {
+            console.log('Server running in port', this.port);
+        });
+    }
+}
+
+export default Server;
